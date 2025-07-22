0409ea7785bcba883891fef53f95a1464d630528
JobMutugi <creativejobu@gmail.com>
2025-07-22 13:08:02 +0300

    add JobCard.jsx

---
+++ My-App/src/Components/JobCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const JobCard = ({ job }) => {
+
+  return (
+    <div style={{ border: '1px solid black', margin: '20px', padding: '10px' }}>
+      <h3>{job.title}</h3>
+      <p><strong>Company:</strong> {job.company}</p>
+      <p><strong>Description:</strong> ${job.description}</p>
+      <p><strong>Category:</strong> ${job.category}</p>
+<p><strong>Status:</strong> {job.applied ? "Applied" : "Not Applied"}</p>
+
+    <button>Edit</button>
+    <button>
+      Delete
+    </button>
+  </div>
+  );
+};
+
+export default GoalCard;
+
